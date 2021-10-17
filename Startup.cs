46e23bbe5830e05e60d6970f9bd7b6eb3735bd4c
Lańczyk Bartłomiej <bartlomiej.lanczyk@microsoft.wsei.edu.pl>
2021-10-17 10:26:24 +0200

    Passing data to Views

--- Startup.cs
+++ Startup.cs
@@ -63,7 +63,7 @@ namespace LibApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{bookId?}");
                 endpoints.MapRazorPages();
             });
         }
