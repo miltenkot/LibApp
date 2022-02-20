448540bf9064baa2eebce72596daaeb20ad30e20
miltenkot <miltenkot@gmail.com>
2022-02-20 19:54:08 +0100

    3. Add database interface.

---
+++ Interfaces/IBookInterface.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using LibApp.Models;
+
+namespace LibApp.Interfaces
+{
+    public interface IBookInterface
+    {
+        IEnumerable<Book> GetBooks();
+
+        Book GetBookById(int bookId);
+
+        void addBook(Book book);
+
+        void UpdateBook(IBookInterface book);
+
+        void deleteBook(int bookId);
+
+        void Save();
+    }
+}
---
+++ Repositories/BookRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using LibApp.Data;
+using System.Collections.Generic;
+using System.Text;
+using LibApp.Models;
+using LibApp.Interfaces;
+
+namespace LibApp.Repositories
+{
+    public class BookRepository: IBookInterface
+    {
+        private ApplicationDbContext _context;
+
+        public BookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        IEnumerable<Book> IBookInterface.GetBooks()
+        {
+            return _context.Books;
+        }
+         
+        Book IBookInterface.GetBookById(int bookId) => _context.Books.Find(bookId);
+
+        void IBookInterface.addBook(Book book) => _context.Add(book);
+
+        void IBookInterface.UpdateBook(IBookInterface book) => _context.Update(book);
+
+        void IBookInterface.deleteBook(int bookId) => _context.Remove(bookId);
+
+        void IBookInterface.Save() => _context.SaveChanges();
+    }
+}
