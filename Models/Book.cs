f04728ddda7c7424b0c36a9dbeaaa746fd7137d2
miltenkot <miltenkot@gmail.com>
2022-02-20 19:05:54 +0100

    2A. Add required alert messages.

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,17 +9,27 @@ namespace LibApp.Models
     public class Book
     {
         public int Id { get; set; }
-		[Required]
+
+		[Required(ErrorMessage = "Name required")]
 		[StringLength(255)]
 		public string Name { get; set; }
-		[Required]
+
+		[Required(ErrorMessage = "Name is required")]
 		public string AuthorName { get; set; }
-		[Required]
+
 		public Genre Genre { get; set; }
+
 		public byte GenreId { get; set; }
+
 		public DateTime DateAdded { get; set; }
+
+		[Required(ErrorMessage = "Date is required")]
 		public DateTime ReleaseDate { get; set; }
+
+		[Required(ErrorMessage = "stocks are required")]
 		public int NumberInStock { get; set; }
+
+		[Required]
 		public int NumberAvailable { get; set; }
 	}
       
